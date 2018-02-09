9a830c243832a4d75c5099f50c988bb46910ed75
Tomas Tomecek <ttomecek@redhat.com>
2018-02-09 16:11:43 +0100

    add a way to load initial fixtures
    Fixes #5
    
    Signed-off-by: Tomas Tomecek <ttomecek@redhat.com>

--- files/init_db.sh
+++ files/init_db.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+set -x
 python3 ./manage.py migrate
 python3 ./manage.py loaddata default_tags
 python3 ./manage.py loaddata default_states
 python3 ./manage.py loaddata default_projects
+test -n "${INITIAL_FIXTURE}" && python3 ./manage.py loaddata $INITIAL_FIXTURE
